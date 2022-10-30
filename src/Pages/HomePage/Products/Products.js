1f79a04da0503a69dca7267b685bf8d2bfc9e4cc
Rajukst <rajuahamedkst15@gmail.com>
2022-10-30 10:51:14 +0600

    done

--- src/Pages/HomePage/Products/Products.js
+++ src/Pages/HomePage/Products/Products.js
@@ -1,41 +1,32 @@
-import React, { useContext } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import CartContext from '../../../AllContexts/Cart/CartContext';
-import Cart from './Cart';
-
-
+import React, { useContext } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import CartContext from "../../../AllContexts/Cart/CartContext";
+import Cart from "./Cart";
 
 const Products = () => {
-const {cartItems, removeItem}= useContext(CartContext);
-    return (
-      <Container fluid>
-        <Row>
-      <Col className='rowi-one' xs={12} md={8} lg={8}>
+  const { cartItems, removeItem } = useContext(CartContext);
+  return (
+    <Container fluid>
       <Row>
-      {cartItems.length === 0 ? (
-  <h4>Cart is Empty</h4>
-) : (
-  <>
-    {
-      cartItems.map((items, index)=> <Cart
-      key={items._id}
-      cartItem={items}
-      index={index}
-      ></Cart> )
-    }
-  </>
-)}
-      </Row>
-      </Col>
-      <Col className='rowi-ones' xs={12} md={4} lg={4}>
-      <h1>This is Right</h1>
-      </Col>
+        <Col className="rowi-one" xs={12} md={8} lg={8}>
+          <Row>
+            {cartItems.length === 0 ? (
+              <h4>Your Cart is Empty</h4>
+            ) : (
+              <>
+                {cartItems.map((items, index) => (
+                  <Cart key={items._id} cartItem={items} index={index}></Cart>
+                ))}
+              </>
+            )}
+          </Row>
+        </Col>
+        <Col className="rowi-ones" xs={12} md={4} lg={4}>
+          <h1>This is Right</h1>
+        </Col>
       </Row>
     </Container>
-    );
+  );
 };
 
 export default Products;
-
-
-
